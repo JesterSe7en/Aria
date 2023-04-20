4757e53306a09f3617f182c2215849753f1d8593
Alex Chan <alyxchan87@gmail.com>
2023-04-19 21:14:24 -0700

    Set startup project to Sandbox

--- premake5.lua
+++ premake5.lua
@@ -1,7 +1,8 @@
-workspace "Aria"
-architecture "x64"
+workspace("Aria")
+architecture("x64")
+startproject("Sandbox")
 
-configurations {"Debug", "Release", "Dist"}
+configurations({ "Debug", "Release", "Dist" })
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.archtecture}"
 
@@ -12,91 +13,96 @@ IncludeDir["Glad"] = "Aria/vendor/Glad/include"
 IncludeDir["imgui"] = "Aria/vendor/imgui"
 IncludeDir["glm"] = "Aria/vendor/glm"
 
-include "Aria/vendor/GLFW"
-include "Aria/vendor/Glad"
-include "Aria/vendor/imgui"
+include("Aria/vendor/GLFW")
+include("Aria/vendor/Glad")
+include("Aria/vendor/imgui")
 
-project "Aria"
-location "Aria"
-kind "StaticLib"
-language "C++"
-cppdialect "C++17"
-staticruntime "on"
+project("Aria")
+location("Aria")
+kind("StaticLib")
+language("C++")
+cppdialect("C++17")
+staticruntime("on")
 
 targetdir("bin/" .. outputdir .. "/%{prj.name}")
 objdir("bin-int/" .. outputdir .. "/%{prj.name}")
 
-pchheader "ariapch.h"
-pchsource "Aria/src/ariapch.cpp" -- needed for VS
+pchheader("ariapch.h")
+pchsource("Aria/src/ariapch.cpp") -- needed for VS
 
-files {
-    "%{prj.name}/src/**.h", "%{prj.name}/src/**.cpp",
-    "%{prj.name}/vendor/glm/glm/**.hpp", "%{prj.name}/vendor/glm/glm/**.inl"
-}
+files({
+	"%{prj.name}/src/**.h",
+	"%{prj.name}/src/**.cpp",
+	"%{prj.name}/vendor/glm/glm/**.hpp",
+	"%{prj.name}/vendor/glm/glm/**.inl",
+})
 
-includedirs {
-    "%{prj.name}/src", "%{prj.name}/vendor/spdlog/include",
-    "%{IncludeDir.GLFW}", "%{IncludeDir.Glad}", "%{IncludeDir.imgui}",
-    "%{IncludeDir.glm}"
-}
+includedirs({
+	"%{prj.name}/src",
+	"%{prj.name}/vendor/spdlog/include",
+	"%{IncludeDir.GLFW}",
+	"%{IncludeDir.Glad}",
+	"%{IncludeDir.imgui}",
+	"%{IncludeDir.glm}",
+})
 
-links {"GLFW", "Glad", "imgui", "opengl32.lib"}
+links({ "GLFW", "Glad", "imgui", "opengl32.lib" })
 
-filter "system:windows"
-systemversion "latest"
+filter("system:windows")
+systemversion("latest")
 
-defines {"ARIA_PLATFORM_WINDOWS", "ARIA_BUILD_DLL"}
+defines({ "ARIA_PLATFORM_WINDOWS", "ARIA_BUILD_DLL" })
 
-filter "configurations:Debug"
-defines "ARIA_DEBUG"
-runtime "Debug"
-symbols "On"
+filter("configurations:Debug")
+defines("ARIA_DEBUG")
+runtime("Debug")
+symbols("On")
 
-filter "configurations:Release"
-defines "ARIA_RELEASE"
-runtime "Release"
-optimize "On"
+filter("configurations:Release")
+defines("ARIA_RELEASE")
+runtime("Release")
+optimize("On")
 
-filter "configurations:Dist"
-defines "ARIA_DIST"
-runtime "Release"
-optimize "On"
+filter("configurations:Dist")
+defines("ARIA_DIST")
+runtime("Release")
+optimize("On")
 
 -- filter { "system:windows", "configurations:Release"}
 --   buildoptions "/MT"
 
-project "Sandbox"
-location "Sandbox"
-kind "ConsoleApp"
-language "C++"
-cppdialect "C++17"
-staticruntime "On"
+project("Sandbox")
+location("Sandbox")
+kind("ConsoleApp")
+language("C++")
+cppdialect("C++17")
+staticruntime("On")
 
 targetdir("bin/" .. outputdir .. "/%{prj.name}")
 objdir("bin-int/" .. outputdir .. "/%{prj.name}")
 
-files {"%{prj.name}/src/**.h", "%{prj.name}/src/**.cpp"}
+files({ "%{prj.name}/src/**.h", "%{prj.name}/src/**.cpp" })
 
-includedirs {"Aria/vendor/spdlog/include", "Aria/src", "%{IncludeDir.glm}"}
+includedirs({ "Aria/vendor/spdlog/include", "Aria/src", "%{IncludeDir.glm}" })
 
-links {"Aria"}
+links({ "Aria" })
 
-filter "system:windows"
-systemversion "latest"
+filter("system:windows")
+systemversion("latest")
 
-defines {"ARIA_PLATFORM_WINDOWS"}
+defines({ "ARIA_PLATFORM_WINDOWS" })
 
-filter "configurations:Debug"
-defines "ARIA_DEBUG"
-runtime "Debug"
-symbols "On"
+filter("configurations:Debug")
+defines("ARIA_DEBUG")
+runtime("Debug")
+symbols("On")
 
-filter "configurations:Release"
-defines "ARIA_RELEASE"
-runtime "Release"
-optimize "On"
+filter("configurations:Release")
+defines("ARIA_RELEASE")
+runtime("Release")
+optimize("On")
 
-filter "configurations:Dist"
-defines "ARIA_DIST"
-runtime "Release"
-optimize "On"
+filter("configurations:Dist")
+defines("ARIA_DIST")
+runtime("Release")
+optimize("On")
