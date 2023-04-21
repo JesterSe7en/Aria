4ef47c76c2201c9fbb3e30d7a32fc7e54141423a
Alex Chan <alyxchan87@gmail.com>
2023-04-20 20:16:35 -0700

    Print out OpenGL render information

--- Aria/src/Platform/OpenGL/OpenGLContext.cpp
+++ Aria/src/Platform/OpenGL/OpenGLContext.cpp
@@ -19,6 +19,17 @@ void OpenGLContext::Init() {
   ARIA_CORE_ASSERT(version, "Failed to initialize Glad");
   ARIA_CORE_INFO("Loaded Glad {0}.{1}", GLAD_VERSION_MAJOR(version),
                  GLAD_VERSION_MINOR(version));
+
+  ARIA_CORE_INFO("OpenGL Info:");
+  // from spdlog
+  //
+  // Formatting of arbitrary pointers is disallowed. If you want to output
+  // a pointer cast it to "void *" or "const void *". In particular, this
+  // forbids formatting of "[const] volatile char *" which is printed as bool
+  // by iostreams
+  ARIA_CORE_INFO("  Vendor: {0}", (const char*)glGetString(GL_VENDOR));
+  ARIA_CORE_INFO("  Renderer: {0}", (const char*)glGetString(GL_RENDERER));
+  ARIA_CORE_INFO("  Version: {0}", (const char*)glGetString(GL_VERSION));
 }
 
 void OpenGLContext::SwapBuffers() { glfwSwapBuffers(m_WindowHandle); }
