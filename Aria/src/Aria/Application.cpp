eb9258bfe7fd7e59f8f068d54d11dd48a5ce1659
Alex Chan <alyxchan87@gmail.com>
2023-04-21 12:46:01 -0700

    Make a less eye searing clear color

--- Aria/src/Aria/Application.cpp
+++ Aria/src/Aria/Application.cpp
@@ -57,7 +57,7 @@ Application::~Application() {}
 
 void Application::Run() {
   while (m_Running) {
-    glClearColor(1, 0, 1, 1);
+    glClearColor(0.1, 0, 0.1, 1);
     glClear(GL_COLOR_BUFFER_BIT);
 
     for (Layer* layer : m_LayerStack) {
