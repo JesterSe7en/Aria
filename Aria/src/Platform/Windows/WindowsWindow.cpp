89d2abed49be458624e5b1a47701f14068664573
Alex Chan <alyxchan87@gmail.com>
2023-04-22 14:28:50 -0700

    Specifically ask for OpenGL 4.6 with window hint

--- Aria/src/Platform/Windows/WindowsWindow.cpp
+++ Aria/src/Platform/Windows/WindowsWindow.cpp
@@ -56,9 +56,9 @@ void WindowsWindow::Init(const WindowProps& props) {
   }
 
   // If not specified, will load latest from graphics driver
-  //glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-  //glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
-  //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
   m_Window = glfwCreateWindow((int)props.Width, (int)props.Height,
                               props.Title.c_str(), nullptr, nullptr);
