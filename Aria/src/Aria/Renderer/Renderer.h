6825bee752a8c5010f786472541475448d93521d
Alex Chan <alyxchan87@gmail.com>
2023-04-27 11:43:53 -0700

    Minor rename

--- Aria/src/Aria/Renderer/Renderer.h
+++ Aria/src/Aria/Renderer/Renderer.h
@@ -2,13 +2,14 @@
 
 #include "RendererAPI.h"
 #include "VertexArray.h"
+#include "RenderCommand.h"
 
 namespace ARIA {
 class Renderer {
  public:
-  static void BeginScene();
-  static void EndScene();
-  static void Submit(const std::shared_ptr<VertexArray>& vertex_array);
+  static void begin_scene();
+  static void end_scene();
+  static void submit(const std::shared_ptr<VertexArray>& vertex_array);
 
   inline static RendererAPI::API get_api() { return RendererAPI::get_api(); }
 };
