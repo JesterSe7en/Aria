940ba4e93dbfb1e70d464fff198a42e0812d649f
Alex Chan <alyxchan87@gmail.com>
2023-05-02 15:13:35 -0700

    Specify c/cpp compiler for cmake generation batch file
    Use clang - maybe in the future add the capability to pass in compiler preference

--- generate_cmake_ninja_multi_config.bat
+++ generate_cmake_ninja_multi_config.bat
@@ -1,3 +1,3 @@
-call cmake -S . -B .\build -G "Ninja Multi-Config" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+call cmake -S . -B .\build -G "Ninja Multi-Config" -D CMAKE_EXPORT_COMPILE_COMMANDS=ON -D CMAKE_C_COMPILER=clang -D CMAKE_CXX_COMPILER=clang++
 pause
 
