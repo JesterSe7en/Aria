e249dcbf249cbd1f34076cdab652d4e17e0c5f22
Alex Chan <alyxchan87@gmail.com>
2023-05-22 16:07:37 -0700

    WIP - VulkanInstance abstraction impl

--- Aria/src/Platform/Vulkan/VulkanInstance.cpp
+++ Aria/src/Platform/Vulkan/VulkanInstance.cpp
@@ -1,12 +1,60 @@
-//
-// Created by alyxc on 5/22/2023.
-//
-
 #include "VulkanInstance.h"
-VulkanInstance::VulkanInstance(VulkanInstance::CreateInfo &create_info) {
+
+namespace aria {
+
+uint32_t VulkanInstance::app_version_ = VK_MAKE_VERSION(1, 0, 0);
+uint32_t VulkanInstance::engine_version_ = VK_MAKE_VERSION(1, 0, 0);
+uint32_t VulkanInstance::api_version_ = VK_VERSION_1_3;
+
+std::vector<const char *> VulkanInstance::validation_layers_ = {"VK_LAYER_KHRONOS_validation"};
+
+VulkanInstance::VulkanInstance(VulkanInstance::VulkanInstanceCreateInfo &create_info) {
+
+//  vkEnumerateInstanceLayerProperties()
+//
+//
+//  VkApplicationInfo app_info;
+//  app_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+//  app_info.pApplicationName = "Aria Vulkan Application";
+//  app_info.applicationVersion = app_version_;
+//  app_info.pEngineName = "Aria";
+//  app_info.engineVersion = engine_version_;
+//  app_info.apiVersion = api_version_;
+//
+//  VkInstanceCreateInfo vk_instance_create_info;
+//  vk_instance_create_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+//  vk_instance_create_info.flags = 0;
+//  vk_instance_create_info.pApplicationInfo = &app_info;
 
 }
-bool VulkanInstance::GetInstanceExtensions(std::string_view layer_name,
+
+VulkanInstance::~VulkanInstance() {
+
+}
+
+bool VulkanInstance::GetInstanceExtensions(const char *layer_name,
                                            std::vector<VkExtensionProperties> &instance_extensions) {
   return false;
+}
+
+
+Ref<VulkanInstance> VulkanInstance::Create(VulkanInstance::VulkanInstanceCreateInfo &create_info) {
+  auto instance = VulkanInstance(create_info);
+  return Ref<VulkanInstance>{instance};
+}
+
+bool VulkanInstance::IsLayerAvailable(const char* layer_name) {
+
+  for (const auto& layer_name: available_layers_) {
+    if (strcmp(layer_name, ))
+  }
+  return false;
+}
+
+void  VulkanInstance::EnumerateLayerProperties() {
+  std::uint32_t layer_count;
+  vkEnumerateInstanceLayerProperties(&layer_count, nullptr);
+
+  vkEnumerateInstanceLayerProperties(&layer_count, available_layers_.data());
+}
 }
--- Aria/src/Platform/Vulkan/VulkanInstance.h
+++ Aria/src/Platform/Vulkan/VulkanInstance.h
@@ -1,21 +1,44 @@
 #pragma once
 
+#include "Aria/Core/Base.h"
 #include "vulkan/vulkan_core.h"
+
+namespace aria {
 
 class VulkanInstance {
  public:
-  struct CreateInfo {
+  struct VulkanInstanceCreateInfo {
     bool enable_validation;
     uint32_t layer_count;
-    std::vector<const char*> pp_layer_names;
+    std::vector<const char *> pp_layer_names;
     uint32_t extension_count;
-    std::vector<const char*> pp_extension_names;
+    std::vector<const char *> pp_extension_names;
     VkDebugUtilsMessengerCreateInfoEXT debug_messenger_create_info;
   };
 
-  VulkanInstance(CreateInfo& create_info);
+  ~VulkanInstance();
 
-  bool GetInstanceExtensions(std::string_view layer_name, std::vector<VkExtensionProperties>& instance_extensions);
-//  std::shared_ptr<VulkanInstance> Create
+  VulkanInstance(const VulkanInstance &) = delete;
+  VulkanInstance &operator=(const VulkanInstance &) = delete;
+
+  static Ref<VulkanInstance> Create(VulkanInstanceCreateInfo &create_info);
+
+  bool GetInstanceExtensions(const char *layer_name, std::vector<VkExtensionProperties> &instance_extensions);
+
  private:
+  static uint32_t app_version_;
+  static uint32_t engine_version_;
+  static uint32_t api_version_;
+
+  static std::vector<const char *> validation_layers_;
+
+  std::vector<VkLayerProperties> available_layers_;
+
+  bool IsLayerAvailable(const char* layer_name);
+  void EnumerateLayerProperties();
+
+
+  explicit VulkanInstance(VulkanInstanceCreateInfo &create_info);
+  VkInstance vk_instance_ = VK_NULL_HANDLE;
 };
+}
