28d1f6b2db5173cf9416812cd71b746f4d266bd7
Alex Chan <alyxchan87@gmail.com>
2023-05-31 22:14:05 -0700

    WIP - Fix vulkan function

--- Aria/src/Platform/Vulkan/VulkanFunction.cpp
+++ Aria/src/Platform/Vulkan/VulkanFunction.cpp
@@ -3,49 +3,90 @@
 #include "Aria/Core/Log.h"
 #include <vulkan/vulkan.h>
 
-template<typename FuncType>
-class VulkanFunction {
- public:
-  static FuncType Load(const char *name) {
-#ifdef _WIN32
-    return reinterpret_cast<FuncType>(GetProcAddress(VulkanLibrary(), name));
-#else
-    return reinterpret_cast<FuncType>(dlsym(VulkanLibrary(), name));
-#endif
-  }
+namespace aria {
 
-  ~VulkanFunction() {
-    // Clean up and unload the Vulkan library
+template<typename FuncType>
+FuncType VulkanFunction<FuncType>::Load(const char *name) {
+#ifdef _WIN32
+  return reinterpret_cast<FuncType>(GetProcAddress(VulkanLibrary(), name));
+#else
+  return reinterpret_cast<FuncType>(dlsym(VulkanLibrary(), name));
+#endif
+}
+
+template<typename FuncType>
+VulkanFunction<FuncType>::~VulkanFunction() {
+  // Clean up and unload the Vulkan library
 #ifdef _WIN32
   FreeLibrary(static_cast<HMODULE>(vkCreateInstancePtr::VulkanLibrary()));
 #else
   dlclose(vkCreateInstancePtr::VulkanLibrary());
 #endif
-  }
+}
 
- private:
-  static void *VulkanLibrary() {
+template<typename FuncType>
+void *VulkanFunction<FuncType>::VulkanLibrary() {
 #ifdef _WIN32
-    static HMODULE library = LoadLibrary(TEXT("vulkan-1.dll"));
+  static HMODULE library = LoadLibrary(TEXT("vulkan-1.dll"));
 #elif defined(__APPLE__)
-    static void *library = dlopen("libvulkan.dylib", RTLD_NOW | RTLD_LOCAL);
-    if (!library) library = dlopen("libvulkan.1.dylib", RTLD_NOW | RTLD_LOCAL);
+      static void *library = dlopen("libvulkan.dylib", RTLD_NOW | RTLD_LOCAL);
+      if (!library) library = dlopen("libvulkan.1.dylib", RTLD_NOW | RTLD_LOCAL);
 #elif defined(__linux__)
-    static void *library = dlopen("libvulkan.so.1", RTLD_NOW | RTLD_LOCAL);
-    if (!library) library = dlopen("libvulkan.so", RTLD_NOW | RTLD_LOCAL);
+      static void *library = dlopen("libvulkan.so.1", RTLD_NOW | RTLD_LOCAL);
+      if (!library) library = dlopen("libvulkan.so", RTLD_NOW | RTLD_LOCAL);
 #else
-    ARIA_CORE_ASSERT(false, "Unsupported platform")
+      ARIA_CORE_ASSERT(false, "Unsupported platform")
 #endif
-    if (library == nullptr) {
-      ARIA_CORE_ASSERT(false, "Failed to load Vulkan runtime library")
-    }
-    return library;
+  if (library == nullptr) {
+    ARIA_CORE_ASSERT(false, "Failed to load Vulkan runtime library")
   }
-};
+  return library;
+}
 
-// Declare function pointers using the template for Vulkan API functions you plan to use
-using PFN_vkCreateInstance = VkResult(VKAPI_PTR *)(const VkInstanceCreateInfo *pCreateInfo, const VkAllocationCallbacks *pAllocator, VkInstance *pInstance);
-using vkCreateInstancePtr = VulkanFunction<PFN_vkCreateInstance>;
+//
+//template<typename FuncType>
+//class VulkanFunction {
+// public:
+//  static FuncType Load(const char *name) {
+//#ifdef _WIN32
+//    return reinterpret_cast<FuncType>(GetProcAddress(VulkanLibrary(), name));
+//#else
+//    return reinterpret_cast<FuncType>(dlsym(VulkanLibrary(), name));
+//#endif
+//  }
+//
+//  ~VulkanFunction() {
+//    // Clean up and unload the Vulkan library
+//#ifdef _WIN32
+//  FreeLibrary(static_cast<HMODULE>(vkCreateInstancePtr::VulkanLibrary()));
+//#else
+//  dlclose(vkCreateInstancePtr::VulkanLibrary());
+//#endif
+//  }
+//
+// private:
+//  static void *VulkanLibrary() {
+//#ifdef _WIN32
+//    static HMODULE library = LoadLibrary(TEXT("vulkan-1.dll"));
+//#elif defined(__APPLE__)
+//    static void *library = dlopen("libvulkan.dylib", RTLD_NOW | RTLD_LOCAL);
+//    if (!library) library = dlopen("libvulkan.1.dylib", RTLD_NOW | RTLD_LOCAL);
+//#elif defined(__linux__)
+//    static void *library = dlopen("libvulkan.so.1", RTLD_NOW | RTLD_LOCAL);
+//    if (!library) library = dlopen("libvulkan.so", RTLD_NOW | RTLD_LOCAL);
+//#else
+//    ARIA_CORE_ASSERT(false, "Unsupported platform")
+//#endif
+//    if (library == nullptr) {
+//      ARIA_CORE_ASSERT(false, "Failed to load Vulkan runtime library")
+//    }
+//    return library;
+//  }
+//};
+//
+//// Declare function pointers using the template for Vulkan API functions you plan to use
+//using PFN_vkCreateInstance = VkResult(VKAPI_PTR *)(const VkInstanceCreateInfo *pCreateInfo, const VkAllocationCallbacks *pAllocator, VkInstance *pInstance);
+//using vkCreateInstancePtr = VulkanFunction<PFN_vkCreateInstance>;
 
 //// Example usage
 //int main() {
@@ -67,3 +108,4 @@ using vkCreateInstancePtr = VulkanFunction<PFN_vkCreateInstance>;
 //
 //  return 0;
 //}
+}// namespace aria
--- Aria/src/Platform/Vulkan/VulkanFunction.h
+++ Aria/src/Platform/Vulkan/VulkanFunction.h
@@ -1,16 +1,18 @@
 #pragma once
-#include <iostream>
 #include <vulkan/vulkan.h>
 
+namespace aria {
 template<typename FuncType>
 class VulkanFunction {
-public:
-    static FuncType Load(const char* name);
+ public:
+  static FuncType Load(const char *name);
+  ~VulkanFunction();
 
-private:
-    static void* VulkanLibrary();
+ private:
+  static void *VulkanLibrary();
 };
 
 // Declare function pointers using the template for Vulkan API functions you plan to use
-using PFN_vkCreateInstance = VkResult(VKAPI_PTR*)(const VkInstanceCreateInfo* pCreateInfo, const VkAllocationCallbacks* pAllocator, VkInstance* pInstance);
+using PFN_vkCreateInstance = VkResult(VKAPI_PTR *)(const VkInstanceCreateInfo *pCreateInfo, const VkAllocationCallbacks *pAllocator, VkInstance *pInstance);
 using vkCreateInstancePtr = VulkanFunction<PFN_vkCreateInstance>;
+}// namespace aria
