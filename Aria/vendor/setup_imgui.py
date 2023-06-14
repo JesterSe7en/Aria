199a1d8ba5109bbabfbdf594fdc587889cacfa6b
Alex Chan <alyxchan87@gmail.com>
2023-06-14 15:32:58 -0700

    Use python3 if building under linux system, fix styling in python script

--- Aria/vendor/setup_imgui.py
+++ Aria/vendor/setup_imgui.py
@@ -1,3 +1,7 @@
+"""
+Build script for ImGui
+"""
+
 import os
 import shutil
 
@@ -13,7 +17,6 @@ files = [
     "imstb_rectpack.h",
     "imstb_textedit.h",
     "imstb_truetype.h",
-
     os.path.join("backends", "imgui_impl_glfw.h"),
     os.path.join("backends", "imgui_impl_glfw.cpp"),
     os.path.join("backends", "imgui_impl_opengl3_loader.h"),
@@ -21,21 +24,14 @@ files = [
     os.path.join("backends", "imgui_impl_opengl3.h"),
     os.path.join("backends", "imgui_impl_vulkan.cpp"),
     os.path.join("backends", "imgui_impl_vulkan.h"),
-    # "backends\\imgui_impl_glfw.cpp",
-    # "backends\\imgui_impl_glfw.h",
-    # "backends\\imgui_impl_opengl3_loader.h",
-    # "backends\\imgui_impl_opengl3.cpp",
-    # "backends\\imgui_impl_opengl3.h",
-    # "backends\\imgui_impl_vulkan.cpp",
-    # "backends\\imgui_impl_vulkan.h",
 ]
 
-src_dir = "imgui"
-dest_dir = "AriaImgui"
-cmake_filename = "AriaImguiCMakeLists.txt"
+SRC_DIR = "imgui"
+DEST_DIR = "AriaImgui"
+CMAKE_FILENAME = "AriaImguiCMakeLists.txt"
 current_dir = os.path.abspath(os.path.dirname(__file__))
-src = os.path.join(current_dir, src_dir)
-dest = os.path.join(current_dir, dest_dir)
+src = os.path.join(current_dir, SRC_DIR)
+dest = os.path.join(current_dir, DEST_DIR)
 
 if not os.path.exists(dest):
     os.mkdir(dest)
@@ -47,18 +43,17 @@ for file in files:
         print("Copying file: " + file)
         new_dest = os.path.join(dest, file)
         t = os.path.basename(new_dest)
-        # get parent directory
         parent = os.path.basename(os.path.dirname(new_dest))
-        if parent != dest_dir:
+        if parent != DEST_DIR:
             # from relative path, remove parent directory
-            shutil.copyfile(os.path.join(src, file),
-                            os.path.join(dest, os.path.basename(file)))
+            shutil.copyfile(
+                os.path.join(src, file), os.path.join(dest, os.path.basename(file))
+            )
         else:
-            shutil.copyfile(os.path.join(src, file),  os.path.join(dest, file))
+            shutil.copyfile(os.path.join(src, file), os.path.join(dest, file))
 
-cmake_filepath = os.path.join(current_dir, cmake_filename)
+cmake_filepath = os.path.join(current_dir, CMAKE_FILENAME)
 if not os.path.exists(cmake_filepath):
     print("Canot find AriaImguiCMakeLists.txt")
 else:
-    # rename it to just CMakeLists.txt and copy it
     shutil.copyfile(cmake_filepath, os.path.join(dest, "CMakeLists.txt"))
