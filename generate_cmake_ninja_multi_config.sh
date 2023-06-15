508e75c53d7dc2de66f5d6d979c230dfa49ae9cb
Alex Chan <alyxchan87@gmail.com>
2023-06-15 15:50:01 -0700

    using wrong clang executable to compile

--- generate_cmake_ninja_multi_config.sh
+++ generate_cmake_ninja_multi_config.sh
@@ -1,1 +1,3 @@
-cmake -S . -B ./build -G "Ninja Multi-Config" -D CMAKE_EXPORT_COMPILE_COMMANDS=ON -D CMAKE_C_COMPILER=clang-16 -D CMAKE_CXX_COMPILER=clang-cpp-16
+#!/bin/bash
+
+cmake -S . -B ./build -G "Ninja Multi-Config" -D CMAKE_EXPORT_COMPILE_COMMANDS=ON -D CMAKE_C_COMPILER=clang-16 -D CMAKE_CXX_COMPILER=clang\+\+-16
